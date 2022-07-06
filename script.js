a0deb9e766139121105e46411b5e338c4489dd4f
Niklas Solomon <niklassolomon@gmail.com>
2022-07-05 20:16:19 -0700

    Added js file and  pseudo code

---
+++ script.js
@@ -0,0 +1,49 @@
+// DECLARE current time
+
+// DECLARE current hour
+
+// DECLARE currentDay element
+
+// DECLARE timeEntries container element
+var containerEl;
+// Render a block for each hour of the day
+for(var i = 9; i <= 17; i++){
+
+    var key ="hour-"+i;
+    
+    // Attempt to get the saved data for the hour of the loop
+    var data = "";
+
+    // Compare i to current hour to determine if this hour is in the past, present, or future.
+
+    var template = `
+        <div class = "row">
+            <div>
+                ${i}AM
+            </div>
+            <div>
+                <textarea></textarea>
+            </div>
+            <div>
+                <button data-hour="${i}">Save</button>
+            </div>
+        </div>
+    `;
+    // Append the html to the container element
+}
+
+// Set up a "click" event listener on the container
+containerEl.on("click", "button", function(event) {
+
+    // Fetch the hour from the clicked button's (event.target) "data-hour" attribute
+
+    // Use the hour to create the key for local storage
+
+    // From the clicked button, traverse the DOM to the nearby <textarea> to fetch its value
+
+    // Use the key and the value to save into local storage
+
+});
+
+
+    // Save an hour to local storage
