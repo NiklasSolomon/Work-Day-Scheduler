13183c04656dba511988e41c64d747fa7d20eaf8
Niklas Solomon <niklassolomon@gmail.com>
2022-07-10 16:32:45 -0700

    Starting over, can't figure out how to load to HTML from JS

--- script.js
+++ script.js
@@ -1,44 +1,18 @@
 // DECLARE current date
-var todaysDate = moment().format('dddd, MMM Do YYYY');
+var todaysDate = moment().format('MMMM Do YYYY, h:mm A');
 // DECLARE current hour
-var timeNow = moment().format("H A");
 // DECLARE currentDay element
 $("#currentDay").html(todaysDate);
 
 // DECLARE timeEntries container element
 var containerEl;
 // Render a block for each hour of the day
-for(var i = 9; i <= 17; i++){
-
-    var key ="hour-"+i;
-    
-    // Attempt to get the saved data for the hour of the loop
-    var data = "";
-
-    // Compare i to current hour to determine if this hour is in the past, present, or future.
-
-    var template = `
-        <div class = "row">
-            <div>
-                ${i}AM
-            </div>
-            <div>
-                <textarea></textarea>
-            </div>
-            <div>
-                <button data-hour="${i}">Save</button>
-            </div>
-        </div>
-    `;
-    // Append the html to the container element
-    $(".container").append(row);
-}
 
 // Set up a "click" event listener on the container
 containerEl.on("click", "button", function(event) {
 
     // Fetch the hour from the clicked button's (event.target) "data-hour" attribute
-
+    
     // Use the hour to create the key for local storage
 
     // From the clicked button, traverse the DOM to the nearby <textarea> to fetch its value
@@ -46,6 +20,4 @@ containerEl.on("click", "button", function(event) {
     // Use the key and the value to save into local storage
 
 });
-
-
     // Save an hour to local storage
