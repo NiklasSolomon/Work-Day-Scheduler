d0345a571ce4c4583056ab18deed42a1f282c245
Niklas Solomon <niklassolomon@gmail.com>
2022-07-10 16:56:53 -0700

    Added color coding to the time blocks

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ var todaysDate = moment().format('MMMM Do YYYY, h:mm A');
 $("#currentDay").html(todaysDate);
 
 // DECLARE timeEntries container element
-var containerEl;
-// Render a block for each hour of the day
+var containerEl = $(".containerEl");
 
 // Set up a "click" event listener on the container
 containerEl.on("click", "button", function(event) {
@@ -20,4 +19,24 @@ containerEl.on("click", "button", function(event) {
     // Use the key and the value to save into local storage
 
 });
+
+// Time blocks are colored based on time of day
+function timeColor() {
+    var hour = moment().hours();
+
+$(".time-block").each(function() {
+    var currentHour = parseInt($(this).attr("id"));
+
+    if (currentHour > hour) {
+        $(this).addClass("future");
+    } else if (currentHour === hour) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("past");
+    }
+})
+};
     // Save an hour to local storage
+
+// Call functions
+timeColor();
